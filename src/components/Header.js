f7623d4dac363a5c1a4727e310e24a88fa2b9e93
Willy Camargo <camargowilly@gmail.com>
2017-06-17 19:36:25 -0300

    add actions on Header component
    related #1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button'
 import logo from '../img/logo.svg'
 
-const Header = () => (
+const Header = ({ isUserLoggedIn }) => (
   <header className="header">
     <div className="header__container">
       <div className="header__logo">
         <img src={logo} alt="WLX" width="35" />
       </div>
+
+        {
+          isUserLoggedIn
+          ? (
+            <div className="header__actions">
+              <Button label="Minha conta" url="/account"/>
+              <Button label="Sair" url="/logout"/>
+            </div>
+          )
+          : (
+            <div className="header__actions">
+              <Button label="Entrar" url="/login"/>
+            </div>
+          )
+        }
     </div>
   </header>
 );
+
+Header.propTypes = {
+  isUserLoggedIn: PropTypes.bool
+}
+
+Header.defaultProps = {
+  isUserLoggedIn: false
+}
 
 export default Header;
